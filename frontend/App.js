6d3eccd44feaaf5ea17ace0e36e92f122828daf6
Tomislav Petkovic <t.petkovic@vegait.rs>
2023-07-18 12:47:42 +0200

    Add library to check is there an internet connection. If there isn't, show message.

--- frontend/App.js
+++ frontend/App.js
@@ -3,17 +3,35 @@ import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
 import { func, bool } from 'prop-types';
 import DeviceInfo from 'react-native-device-info';
 import styled from 'styled-components/native';
-import { white } from './src/constants/colors';
+import { black, white } from './src/constants/colors';
 import AppStackNavigator from './src/navigation/AppNavigator';
 import { connect } from 'react-redux';
 import SideBarMenu from './src/components/SideBarMenu/SideBarMenu';
 import { setDeviceId } from './src/redux/actions/deviceIdActions';
 import { navigationRef } from './src/navigation/RootNavigation';
+import { useNetInfo } from '@react-native-community/netinfo';
+import { Text, View } from 'react-native';
 
 const MainContainer = styled.View`
   flex: 1;
   background-color: ${white};
 `;
+
+const NoInternetWrapper = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const NoInternetText = styled.Text`
+  text-align: center;
+  color: black;
+  font-size: 25px;
+  text-transform: uppercase;
+  font-family: 'Roboto';
+  width: 80%;
+`;
+
 const MyTheme = {
   ...DefaultTheme,
   colors: {
@@ -28,12 +46,22 @@ const App = ({ sidebarMenu, setDeviceId }) => {
     });
   }, []);
 
+  const netInfo = useNetInfo();
+  const isConnectedToInternet =
+    netInfo.isConnected || netInfo.isConnected === null;
+
   return (
     <MainContainer>
-      <NavigationContainer ref={navigationRef} theme={MyTheme}>
-        <AppStackNavigator />
-        {sidebarMenu && <SideBarMenu />}
-      </NavigationContainer>
+      {!isConnectedToInternet ? (
+        <NoInternetWrapper>
+          <NoInternetText>Nema internet konekcije</NoInternetText>
+        </NoInternetWrapper>
+      ) : (
+        <NavigationContainer ref={navigationRef} theme={MyTheme}>
+          <AppStackNavigator />
+          {sidebarMenu && <SideBarMenu />}
+        </NavigationContainer>
+      )}
     </MainContainer>
   );
 };
